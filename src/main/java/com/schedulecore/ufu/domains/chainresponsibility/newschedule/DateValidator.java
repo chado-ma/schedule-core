36f6225a79861d9e60be869ace8a15ede6de4cd0
Gabriel Rezende Machado <47564540+grm1003@users.noreply.github.com>
2025-08-31 15:16:18 -0300

    ajustes comparação de data

--- src/main/java/com/schedulecore/ufu/domains/chainresponsibility/newschedule/DateValidator.java
+++ src/main/java/com/schedulecore/ufu/domains/chainresponsibility/newschedule/DateValidator.java
@@ -3,18 +3,24 @@ package com.schedulecore.ufu.domains.chainresponsibility.newschedule;
 import com.schedulecore.ufu.domains.chainresponsibility.ScheduleValidator;
 import com.schedulecore.ufu.domains.models.NewSchedule;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Date;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.*;
 
 public class DateValidator extends ScheduleValidator {
     @Override
     public boolean check(NewSchedule schedule) {
     if (schedule.getData() == null)
         throw new IllegalArgumentException("Date cannot be null");
-    if(schedule.getData().before(Date.from(Instant.now())))
+    if( isAgendamentoNoPassado(schedule.getData(), schedule.getHorario()))
         throw new IllegalArgumentException("Date cannot be in the past, data invalid: " + schedule.getData());
 
     return checkNext(schedule);
+    }
+
+    private boolean isAgendamentoNoPassado(Date data, Time horario) {
+        LocalDate localDate = data.toLocalDate();
+        LocalTime localTime = horario.toLocalTime();
+        return  LocalDateTime.of(localDate, localTime).isBefore(LocalDateTime.now());
     }
 }
